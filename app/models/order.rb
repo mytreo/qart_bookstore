02a6393d3f735192c23c37069deb9462cd3fa25d
mytreo <mytreogm@gmail.com>
2016-04-26 21:49:01 +0300

    first implementation of quantity changes when create|reject order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,20 +4,19 @@ class Order < ActiveRecord::Base
   belongs_to :user
 
   def add_line_items_from_cart(cart)
+      books_and_quantities={}
       cart.line_items.each do |item|
       item.cart_id = nil
+      books_and_quantities.merge({book: item.book,quantity: item.quantity})
       line_items << item
+      if item.quantity > item.book.quantity
+        return false
+      end
     end
   end
 
   def total_price
     line_items.to_a.sum{|item| item.total_price}
-  end
-
-  def change_quantity(items)
-    items.each do |item|
-      item.book.quantity+=9999
-    end
   end
 
 end

5c6462cb9830cb7e6b6300b1b83af1185d94c629
Anton <mytreogm@gmail.com>
2016-04-27 08:29:37 +0300

    hotfix search_string3

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,8 @@ class SearchController < ApplicationController
   def search
     @search_string=params[:search_string]
     @genre_id=params[:genre_ids]
-    @genres =Genre.select("genres.id, genres.name, sum(1) s").joins(:books).where("books.name like ?",('%'+@search_string+'%')).group(:id, :name)
-    @books = Book.where("name like ?", ('%'+@search_string+'%'))
+    @genres =Genre.select("genres.id, genres.name, sum(1) s").joins(:books).where("books.name like ?", "%#{@search_string}%").group(:id, :name)
+    @books = Book.where("name like ?", "%#{@search_string}%")
     if @genre_id
       @books = @books.where("genre_id in (?)", @genre_id.to_i)
     end
